a12b53f9f35ac8d26f826f2e27642469ca1d5419
Faye Carter <fayecarter333@gmail.com>
2020-03-24 12:58:24 +0000

    BirthdayList.store returns Birthday

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,6 +1,7 @@
 class BirthdayList
 
   def store(name, birthday)
+    [{ name: 'Faye Carter', birthday: "7 April 1990" }]
   end
-  
+
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -7,4 +7,8 @@ describe BirthdayList do
     expect(list).to respond_to(:store).with(2).arguments
   end
 
+  it '#store can store a Birthday' do
+    expect(list.store('Faye Carter', "7 April 1990")).to eq [{ name: 'Faye Carter', birthday: "7 April 1990" }]
+  end
+
 end
